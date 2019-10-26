2fbbb9076b0b5ccf095b28a9a15ef68c29acb974
Thomas Mansencal <thomas.mansencal@gmail.com>
2019-10-26 17:22:13 +1300

    Clean rebuild.

--- output/assets/js/custom.js
+++
@@ -1,6 +0,0 @@
-/*** Enable hover on dropdown menu. ***/
-$('ul.navbar-nav li.dropdown').hover(function () {
-    $(this).find('.dropdown-menu').stop(true, true).delay(50).fadeIn(100);
-}, function () {
-    $(this).find('.dropdown-menu').stop(true, true).delay(50).fadeOut(100);
-});
